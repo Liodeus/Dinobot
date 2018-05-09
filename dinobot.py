fdf4ac42b8a9005530cf4c29d11ddd324f4e453d
Liodeus <thibaultgalbourdin@live.fr>
2018-05-09 23:00:14 +0200

    Add command dino and translate

--- dinobot.py
+++ dinobot.py
@@ -2,6 +2,7 @@ import discord
 from discord.ext.commands import Bot
 from discord.ext import commands
 from random import randint
+from time import sleep
 import asyncio
 
 
@@ -9,41 +10,48 @@ bot = commands.Bot(command_prefix = "!")
 
 @bot.event
 async def on_ready():
-	messages = ["Grrrrrr Dinobot arrive sur le serveur !", "Dinobot arrive pour bouffer vos daronnes !"]
-	for server in bot.servers:
-		lstChannel = list(server.channels)
-		lstChannelName = [x.name for x in list(server.channels)]
-		try:
-			index = lstChannelName.index("general")
-			print(f"Join {server}")
-			message = messages[randint(0, len(messages) - 1)]
-			print(f"{message}\n")
-			await bot.send_message(lstChannel[index], message)
-		except:
-			print(f"Pas de channel \"general\" sur {server}")
+	print("co")
+	messages = ["Grrrrrr Dinobot arrive sur le serveur !", "Dinobot arrive pour bouffer vos daronnes !", "Dinobot ici pour casser du CDAISI !"]
+#	for server in bot.servers:
+#		lstChannel = list(server.channels)
+#		lstChannelName = [x.name for x in list(server.channels)]
+#		try:
+#			index = lstChannelName.index("general")
+#			print(f"Join {server}")
+#			message = messages[randint(0, len(messages) - 1)]
+#			print(f"{message}")
+#			await bot.send_message(lstChannel[index], message)
+#		except:
+#			print(dinosay("Pas de channel \"general\" sur {server}"))
+
+@bot.command(pass_context=True)
+async def dino(ctx):
+	print(f"{ctx.message.author} : {ctx.message.content}")
+	print(dinosay("grrrrrr"))
+	await bot.say("grrrrrr")
 
 
-@bot.event
-async def on_message(message):
-	if message.content.upper() == "!DINO":
-		await bot.send_message(message.channel, "grrrrrr")
-	if message.content.upper().startswith("!TRANSLATE"):
-		if len(message.content) > 10:
-			print(f"\n{message.author} : {message.content}\n")
-			strg = ' '.join(message.content.split(' ')[1:])
-			newStrg = ""
-			for x in strg:
-				print("x= ", x)
-				if x != ' ':
-					newStrg += chr(randint(65, 122))
-				else:
-					newStrg += ' '
-			await bot.send_message(message.channel, newStrg)
-		else:
-			error = "Il manque le message Dino !"
-			print(f"{error}\n")
-			await bot.send_message(message.channel, error)
+@bot.command(pass_context=True)
+async def translate(ctx):
+	if len(ctx.message.content) > 10:
+		print(f"\n{ctx.message.author} : {ctx.message.content}")
+		strg = ' '.join(ctx.message.content.split(' ')[1:])
+		newStrg = ""
+		for x in strg:
+			if x != ' ':
+				newStrg += chr(randint(65, 122))
+			else:
+				newStrg += ' '
+		print(f"Dinobot : {newStrg}")
+		await bot.say(newStrg)
+	else:
+		error = "Il manque le message Dino !"
+		print(dinosay(error))
+		await bot.say(error)
+
+
+def dinosay(strg):
+	return f"Dinobot : {strg}"
 
 
 bot.run("secret")
-
